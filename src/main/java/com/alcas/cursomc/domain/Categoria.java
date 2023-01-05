8d91f17ec7cecc11f9847eb9a78fa00a3190a2ea
andre-alcas <alcas.andre@gmail.com>
2023-01-04 23:22:13 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/alcas/cursomc/domain/Categoria.java
+++ src/main/java/com/alcas/cursomc/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.alcas.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -18,6 +21,17 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
 	public Categoria() {
 		
 	}
