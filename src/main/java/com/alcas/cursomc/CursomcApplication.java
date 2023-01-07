afe3de4b7753c96b34bd39997154a590f1f27adf
andre-alcas <alcas.andre@gmail.com>
2023-01-07 19:51:43 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/alcas/cursomc/CursomcApplication.java
+++ src/main/java/com/alcas/cursomc/CursomcApplication.java
@@ -1,22 +1,32 @@
 package com.alcas.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import com.alcas.cursomc.domain.Categoria;
 import com.alcas.cursomc.domain.Cidade;
 import com.alcas.cursomc.domain.Cliente;
 import com.alcas.cursomc.domain.Endereco;
 import com.alcas.cursomc.domain.Estado;
+import com.alcas.cursomc.domain.Pagamento;
+import com.alcas.cursomc.domain.PagamentoComBoleto;
+import com.alcas.cursomc.domain.PagamentoComCartao;
+import com.alcas.cursomc.domain.Pedido;
 import com.alcas.cursomc.domain.Produto;
+import com.alcas.cursomc.domain.enums.EstadoPagamento;
 import com.alcas.cursomc.domain.enums.TipoCliente;
 import com.alcas.cursomc.repositories.CategoriaRepository;
 import com.alcas.cursomc.repositories.CidadeRepository;
 import com.alcas.cursomc.repositories.ClienteRepository;
 import com.alcas.cursomc.repositories.EnderecoRepository;
 import com.alcas.cursomc.repositories.EstadoRepository;
+import com.alcas.cursomc.repositories.PagamentoRepository;
+import com.alcas.cursomc.repositories.PedidoRepository;
 import com.alcas.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -39,6 +49,13 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -86,6 +103,22 @@ public class CursomcApplication implements CommandLineRunner{
 	    
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 	}
 
--- src/main/java/com/alcas/cursomc/domain/Cliente.java
+++ src/main/java/com/alcas/cursomc/domain/Cliente.java
@@ -27,18 +27,21 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
-	@OneToMany(mappedBy="cliente")
+
+	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
-	
+
 	@ElementCollection
-	@CollectionTable(name="TELEFONE")
+	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
-	
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+
 	public Cliente() {
-		
+
 	}
-	
+
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
 		super();
 		this.id = id;
@@ -51,39 +54,51 @@ public class Cliente implements Serializable {
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getCpfOuCnpj() {
 		return cpfOuCnpj;
 	}
+
 	public void setCpfOuCnpj(String cpfOuCnpj) {
 		this.cpfOuCnpj = cpfOuCnpj;
 	}
+
 	public TipoCliente getTipo() {
 		return TipoCliente.toEnum(tipo);
 	}
+
 	public void setTipo(TipoCliente tipo) {
 		this.tipo = tipo.getCod();
 	}
+
 	public Set<String> getTelefones() {
 		return telefones;
 	}
+
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+
 	public List<Endereco> getEnderecos() {
 		return enderecos;
 	}
@@ -91,11 +106,20 @@ public class Cliente implements Serializable {
 	public void setEnderecos(List<Endereco> enderecos) {
 		this.enderecos = enderecos;
 	}
-	
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -107,5 +131,5 @@ public class Cliente implements Serializable {
 		Cliente other = (Cliente) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
